dc2bea881f8e417a90f2dd91d99678fd4de0d5bb
Wagner.A.S <alvesdasilvawagner1997@gmail.com>
2020-05-24 12:38:23 -0300

    consertando-erro-cores

--- main.js
+++ main.js
@@ -14,11 +14,13 @@ function sinalverde(){
     document.querySelector('#pare').style.color="white"
     document.querySelector('#atencao').style.color="white"
     document.querySelector('#prossiga').style.color="black"
-    count = 5
+    count = 10
     tempo = count*1000
     setTimeout(sinalamarelo,tempo)
 }
 function sinalamarelo(){
+    sinal_verde.style.backgroundColor= "#808080"
+    sinal_verde.style.boxShadow= '0px 0px 44px -10px rgba(130,196,43,0)'
     sinal_amarelo.style.backgroundColor = 'orange'
     sinal_amarelo.style.boxShadow = '0px 0px 44px -10px rgb(206, 246, 25)'
     document.querySelector('#pare').style.color="white"
@@ -29,12 +31,14 @@ function sinalamarelo(){
     setTimeout(sinalvermelho,tempo)
 }
 function sinalvermelho(){
+    sinal_amarelo.style.backgroundColor = "#808080"
+    sinal_amarelo.style.boxShadow = '0px 0px 44px -10px rgb(206, 246, 25, 0)'
     sinal_vermelho.style.backgroundColor = 'red'
     sinal_vermelho.style.boxShadow = '0px 0px 44px -10px rgb(196, 43, 43)'
     document.querySelector('#pare').style.color="black"
     document.querySelector('#atencao').style.color="white"
     document.querySelector('#prossiga').style.color="white"
-    count = 4
+    count = 5
     tempo = count*1000
     setTimeout(sinalverde,tempo)
 }
