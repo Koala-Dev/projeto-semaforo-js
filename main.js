c617c188c4a495d9eafde82fe1c51a34669ab203
Wagner.A.S <alvesdasilvawagner1997@gmail.com>
2020-05-24 12:47:12 -0300

    trocando-mudanca-letra-de-display-para-visibility

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ function sinalverde(){
     sinal_vermelho.style.boxShadow = '0px 0px 44px -10px rgb(196, 43, 43, 0)'
     sinal_verde.style.backgroundColor= 'green'
     sinal_verde.style.boxShadow= '0px 0px 44px -10px rgba(130,196,43,1)'
-    document.querySelector('#pare').style.display="hidden"
-    document.querySelector('#atencao').style.display="hidden"
-    document.querySelector('#prossiga').style.display="show"
+    document.querySelector('#pare').style.visibility ="hidden"
+    document.querySelector('#atencao').style.visibility ="hidden"
+    document.querySelector('#prossiga').style.visibility ="visible"
     count = 10
     tempo = count*1000
     setTimeout(sinalamarelo,tempo)
@@ -23,9 +23,9 @@ function sinalamarelo(){
     sinal_verde.style.boxShadow= '0px 0px 44px -10px rgba(130,196,43,0)'
     sinal_amarelo.style.backgroundColor = 'orange'
     sinal_amarelo.style.boxShadow = '0px 0px 44px -10px rgb(206, 246, 25)'
-    document.querySelector('#pare').style.display="hidden"
-    document.querySelector('#atencao').style.display="show"
-    document.querySelector('#prossiga').style.display="hidden"
+    document.querySelector('#pare').style.visibility ="hidden"
+    document.querySelector('#atencao').style.visibility ="visible"
+    document.querySelector('#prossiga').style.visibility ="hidden"
     count = 2
     tempo = count*1000
     setTimeout(sinalvermelho,tempo)
@@ -35,9 +35,9 @@ function sinalvermelho(){
     sinal_amarelo.style.boxShadow = '0px 0px 44px -10px rgb(206, 246, 25, 0)'
     sinal_vermelho.style.backgroundColor = 'red'
     sinal_vermelho.style.boxShadow = '0px 0px 44px -10px rgb(196, 43, 43)'
-    document.querySelector('#pare').style.display="show"
-    document.querySelector('#atencao').style.display="hidden"
-    document.querySelector('#prossiga').style.display="hidden"
+    document.querySelector('#pare').style.visibility ="visible"
+    document.querySelector('#atencao').style.visibility ="hidden"
+    document.querySelector('#prossiga').style.visibility ="hidden"
     count = 5
     tempo = count*1000
     setTimeout(sinalverde,tempo)
